0253fc1fff1e4f86f9fa7e854621d0b69376d54c
Movileanu Miruna <mirunamovi@gmail.com>
2024-03-21 20:23:48 +0200

    Progress

---
+++ Ionic-Angular/src/app/services/api.service.ts
@@ -0,0 +1,18 @@
+// api.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://localhost:3000/api'; 
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/users`);
+  }
+
+}
