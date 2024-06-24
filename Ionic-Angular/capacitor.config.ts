43802fbf458c16107edc5e59ca08c9f04293d2a8
Movileanu Miruna <mirunamovi@gmail.com>
2024-06-24 23:43:13 +0300

    final

--- Ionic-Angular/capacitor.config.ts
+++ Ionic-Angular/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Ionic-Angular',
+  appName: 'PeakGeek',
   webDir: 'www',
   server: {
     androidScheme: 'http',
